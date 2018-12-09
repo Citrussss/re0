946f5f0cafa465aaeb758109c736249901f9a0fd
bangbang <292837678@qq.com>
2018-12-09 15:39:02 +0800

    Initial commit

---
+++ src/main/java/com/king/re0/Re0Application.java
@@ -0,0 +1,11 @@
+package com.king.re0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Re0Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Re0Application.class, args);
+	}
+}
