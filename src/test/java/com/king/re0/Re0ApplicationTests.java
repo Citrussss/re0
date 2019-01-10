94b969c32a3ae0853716854e2c01b9bfd7caa154
jimby <huangxiujie@email.com>
2019-01-10 11:20:22 +0800

    kai

--- src/test/java/com/king/re0/Re0ApplicationTests.java
+++ src/test/java/com/king/re0/Re0ApplicationTests.java
@@ -22,25 +22,24 @@ public class Re0ApplicationTests {
         S = "ajx37nyx97niysdrzice4petvcvmcgqn282zicpbx6okybw93vhk782unctdbgmcj";
 //                "82unctdbgmcjmbqn25rorktmu5ig2qn2y4xagtru2nehmsp";
         K = 976159153;
-        for (int i = 1; i <= 64; i++) {
-            System.out.print(fun(S.toCharArray(), 0, 0, i, 0));
-        }
+//        for (int i = 1; i <= 64; i++) {
+            System.out.print(fun(S.toCharArray(),0,0,K,0));
+//        }
 //	   System.out.print(fun(S.toCharArray(),0,0,K,0)+
 //               fun(S.toCharArray(),0,0,K,0));
-    }
-
-    private char fun(char[] chars, int start, long lenth, long target, int cycle) {
+	}
+	private char fun(char[] chars,int start,long lenth,long target,int cycle){
 //	    System.out.println("target"+target);
         for (int i = start; i < chars.length; i++) {
             char c = chars[i];
-            if (isShuzi(c)) {
-                long newLenth = (lenth + i - start) * (c - 48);
-                if (newLenth < target) {
+           if(isShuzi(c)){
+               long newLenth = (lenth + i - start) * (c-48);
+                if(newLenth<target){
 //                    System.out.println(newLenth);
-                    return fun(chars, i + 1, newLenth, target, cycle + 1);
-                } else if (target % (lenth + i - start) != 0) return fun(chars, 0, 0, target % (lenth + i - start), 0);
-                else return returnChar(chars, i);
-            } else if (i + lenth - start + 1 == target) return returnChar(chars, i);
+                    return fun(chars,i+1,newLenth,target,cycle+1);
+                }else if(target%(lenth + i - start)!=0)return fun(chars,0,0,target%(lenth + i - start),0);
+                else return returnChar(chars,i);
+            }else if(i+lenth-start+1==target)return returnChar(chars,i);
         }
         return '1';
     }
@@ -49,8 +48,8 @@ public class Re0ApplicationTests {
         return c <= '9' && c >= '0';
     }
 
-    private char returnChar(char[] chars, int position) {
-        return isShuzi(chars[position]) ? returnChar(chars, position - 1) : chars[position];
+    private char returnChar(char[] chars,int position){
+        return isShuzi(chars[position])?returnChar(chars,position-1):chars[position];
     }
 
     ;
